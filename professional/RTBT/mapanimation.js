39aeb722b4a167421a22469649a20a36e19fbf1a
Ekalex <ekalex08@gmail.com>
2021-03-29 16:46:44 -0400

    PacMen functionality

--- professional/RTBT/mapanimation.js
+++ professional/RTBT/mapanimation.js
@@ -1,89 +1,41 @@
-var map;
+mapboxgl.accessToken = 'pk.eyJ1IjoiZWthbGV4MDgiLCJhIjoiY2ttMTNzZmdtMDMxdDJwanZtajMwbzBoaiJ9.Rb0d4g6qLSpGbG8iIyvBJw'
 
-var markers = [];
-const accessToken = 'AIzaSyBK5h2JkwX-dgX1yoUVEnbUWTPQBexEy78';
-
-// load map
-function init(){
-var myOptions = {
-    zoom      : 15,
-    center    : { lat:42.353350,lng:-71.091525},
-    mapTypeId : google.maps.MapTypeId.ROADMAP
-};
-var element = document.getElementById('map');
-  map = new google.maps.Map(element, myOptions);
-  addMarkers();
-}
-
-// Add bus markers to map
-async function addMarkers(){
-// get bus data
-var locations = await getBusLocations();
-
-// loop through data, add bus markers
-locations.forEach(function(bus){
-    var marker = getMarker(bus.id);		
-    if (marker){
-        moveMarker(marker,bus);
-    }
-    else{
-        addMarker(bus);			
-    }
+let map = new mapboxgl.Map({
+    container: "map",
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-71.0942, 42.3601],
+    zoom: 14
 });
 
-// timer
-console.log(new Date());
-setTimeout(addMarkers,15000);
+let busLocations = [];
+async function run(){
+	// get bus data  
+    let location = [];
+	const locations = await getBusLocations();
+	console.log(new Date());
+	console.log(locations);
+	
+	for(let element of locations){
+		let lng = element.attributes.longitude;
+		let lat = element.attributes.latitude;
+		location = [lng, lat];
+		var marker = new mapboxgl.Marker()
+			.setLngLat(location)
+			.addTo(map);
+		}
+    //remove previous markers
+	
+	// timer
+	setTimeout(run, 15000);
 }
 
 // Request bus data from MBTA
 async function getBusLocations(){
-var url = 'https://api-v3.mbta.com/vehicles?api_key=ca34f7b7ac8a445287cab52fb451030a&filter[route]=1&include=trip';	
-var response = await fetch(url);
-var json     = await response.json();
-return json.data;
+	const url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
+	const response = await fetch(url);
+	const json     = await response.json();	
+	
+	return json.data;
 }
 
-function addMarker(bus){
-var icon = getIcon(bus);
-var marker = new google.maps.Marker({
-    position: {
-        lat: bus.attributes.latitude, 
-        lng: bus.attributes.longitude
-    },
-    zoom: 20,
-    map: map,
-    icon: icon,
-    id: bus.id
-});
-markers.push(marker);
-}
-
-function getIcon(bus){
-// select icon based on bus direction
-if (bus.attributes.direction_id === 0) {
-    return 'red.png';
-}
-return 'blue.png';	
-}
-
-function moveMarker(marker,bus) {
-// change icon if bus has changed direction
-var icon = getIcon(bus);
-marker.setIcon(icon);
-
-// move icon to new lat/lon
-marker.setPosition( {
-    lat: bus.attributes.latitude, 
-    lng: bus.attributes.longitude
-});
-}
-
-function getMarker(id){
-var marker = markers.find(function(item){
-    return item.id === id;
-});
-return marker;
-}
-
-window.onload = init;
+run();
